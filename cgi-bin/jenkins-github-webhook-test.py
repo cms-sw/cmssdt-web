877f315020352dcb51d5d10cc5e26b714d5791f0
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-09-28 10:00:27 +0200

    make use of jenkins_callback

--- cgi-bin/jenkins-github-webhook-test.py
+++ cgi-bin/jenkins-github-webhook-test.py
@@ -1,40 +1,13 @@
 #!/usr/bin/env python
+from __future__ import print_function
+from jenkins_callback import build_jobs
 import sys, json
-from _py2with3compatibility import Request, urlopen, urlencode, build_opener, install_opener, CookieJar, HTTPCookieProcessor, HTTPError
 
 try: jenkins_server=sys.argv[1]
 except: jenkins_server="http://cmsjenkins03.cern.ch:8080/jenkins"
-try: login_key=sys.argv[2]
-except: login_key='ADFS_LOGIN'
 
 params = []
-job="jenkins-installation-trigger-cli"
 params.append({"name":"JOB_PARAM1","value":"cmssdt1"})
 params.append({"name":"JOB_PARAM2","value":"cmssdt2"})
-url = jenkins_server+'/job/'+job+'/build'
-jenkins_parameters = json.dumps({"parameter":params})
-print (jenkins_parameters,job)
-data = {
-  "json": jenkins_parameters,
-   "Submit": "Build"
-}
 
-install_opener(build_opener(HTTPCookieProcessor(CookieJar())))
-headers = {login_key : "cmssdt"}
-try:
-  req = Request(url=jenkins_server+'/crumbIssuer/api/json', headers=headers)
-  crumb = json.loads(urlopen(req).read())
-  headers[crumb['crumbRequestField']] = crumb['crumb']
-  print ("OK crumbRequest")
-except HTTPError as e:
-  print ("Running without Crumb Issuer: %s" % e)
-  pass
-
-try:
-  data=urlencode(data)
-  req=Request(url=url,data=data,headers=headers)
-  content=urlopen(req).read()
-  print ("ALL_OK")
-except Exception as e:
-  print ("Unable to start jenkins job: %s" % e)
-
+build_jobs(jenkins_server, [(json.dumps({"parameter":params}),"jenkins-installation-trigger-cli")])
